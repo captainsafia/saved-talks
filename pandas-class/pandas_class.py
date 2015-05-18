40aa9dc9374cc0b3574986dbd619ff4dcc9c5ff8
Safia Abdalla <sabdalla@sapient.com>
2015-05-18 11:04:40 -0500

    Added pandas_class.py script

---
+++ pandas-class/pandas_class.py
@@ -0,0 +1,21 @@
+def welcome():
+	print """
+
+	 _|_|_|      _|_|    _|      _|  _|_|_|      _|_|      _|_|_|  
+	 _|    _|  _|    _|  _|_|    _|  _|    _|  _|    _|  _|        
+	 _|_|_|    _|_|_|_|  _|  _|  _|  _|    _|  _|_|_|_|    _|_|    
+	 _|        _|    _|  _|    _|_|  _|    _|  _|    _|        _|  
+	 _|        _|    _|  _|      _|  _|_|_|    _|    _|  _|_|_|    
+	                                                               
+                                                               
+	"""
+
+def what_is_pandas():
+	print """Pandas is a Python library that provides a set of fast data
+	structures and tools for data analytics!"""
+
+def who_am_i():
+	print "Co-organizer of PyLadies Chicago and data scientist at SapientNitro"
+
+def where_is_the_data():
+	print "http://bit.ly/gapl-ridership"
